2c290484ccfa38ae747971aa22969e9346fe1627
Tereza Smolova <tereza.smolova@gmail.com>
2023-11-06 14:25:44 +0100

    working in progress

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById("menuToggle");
-const menu = document.querySelector(".header__menu");
-
-menuToggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
-});
